43115785ca9d3f1b20d49db5e3f307b5cb3c941f
Timur <gub.timur@gmail.com>
2023-04-17 00:52:42 +0300

    пушу проект

--- archivemanager/filemanagement/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class FilemanagementConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'filemanagement'
--- archivemanager/loginwindow/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from django.contrib.auth.models import User
-from django.contrib.auth.forms import AuthenticationForm
-
-
-class AuthUserForm(AuthenticationForm, forms.ModelForm):
-    class Meta:
-        model = User
-        fields = ('username', 'password')
