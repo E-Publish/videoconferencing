400f5cb818f56d099a4510245161ec1f12d2608f
Natalya Galushko <nat@e-publish.ru>
2023-06-20 11:44:27 +0000

    Revert "Merge branch 'dev/015' into 'main'"
    This reverts merge request !15

--- archivemanager/urls.py
+++ archivemanager/urls.py
@@ -20,7 +20,7 @@ from filemanagement.views import upload_file, unpack_zip, download_file, downloa
     download_protected_archive
 from loginwindow.views import login_page, redirect_to_filemanager
 from simpleuserpage.views import show_archive_data, edit_info, admin_panel, edit_user_info, delete_user, \
-    add_user, search, sort_table, video_player, video_streaming, update_unpacking_progress
+    add_user, search, sort_table, video_player
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -42,7 +42,5 @@ urlpatterns = [
     path('simpleuser/<int:id>/', unpack_zip, name='unpack_archive'),
     path('simpleuser/sort/<str:field>/', sort_table, name='sort_table'),
     path('simpleuser/upload/<int:id>/', upload_file, name='upload_file'),
-    path('video/<int:id>/', video_player, name='video_player'),
-    path('stream/<int:id>/', video_streaming, name='stream'),
-    path('update_info/', update_unpacking_progress, name='update_unpacking_progress'),
+    path('play_video/<int:id>/', video_player, name='video_player'),
 ]
