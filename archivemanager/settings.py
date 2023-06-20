08f783a73261f72d0c9f5f09687a0a2a8e908d9e
Yaroslav <ypklementev@icloud.com>
2023-06-20 12:08:58 +0300

    Динамическое обновление информации, потоковый видеоплеер

--- archivemanager/settings.py
+++ archivemanager/settings.py
@@ -68,10 +68,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'acarchiveinfo',
-        'USER': 'root',
-        'PASSWORD': 'root',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
+        'USER': 'videoarc',
+        'PASSWORD': 'azz78znppcyLph',
+        'HOST': '172.16.1.28',
+        'PORT': '3307',
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
         }
@@ -140,8 +140,8 @@ EMAIL_USE_SSL = True
 MEDIA_ROOT = '../EpublishPath'
 
 
-BBB_VIDEO = 'C:/Users/kleme/Documents/EpublishPath/common'
-DESTINATION = 'C:/Users/kleme/Documents/EpublishPath/restrict'
+BBB_VIDEO = '/opt/videoconferencing.epublish.ru/'
+DESTINATION = '/opt/resctrict/'
 
 
 ZIP_PASSWORD = 'HiperbolA'
